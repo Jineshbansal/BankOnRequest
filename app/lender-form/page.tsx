f86b01e481337ddb19661d18a71166ea5e86bc82
karankoder <karankr.das.cse22@iitbhu.ac.in>
2024-12-03 10:53:25 +0530

    feat: add custom lender form

---
+++ app/lender-form/page.tsx
@@ -0,0 +1,34 @@
+'use client';
+import React, { useState } from 'react';
+
+const App = () => {
+  const [inputValue, setInputValue] = useState('');
+
+  const submitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+    alert('Form is clicked');
+    console.log(inputValue);
+  };
+
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <form onSubmit={submitHandler} className='flex flex-col gap-4'>
+        <input
+          type='text'
+          value={inputValue}
+          placeholder='Enter your name'
+          onChange={(e) => setInputValue(e.target.value)}
+          className='p-2 text-lg border border-gray-300 rounded'
+        />
+        <button
+          type='submit'
+          className='p-2 text-lg bg-blue-500 text-white rounded hover:bg-blue-700'
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
