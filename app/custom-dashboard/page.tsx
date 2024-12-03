591d471fc56c2aac1396dd8718c676e033404ace
karankoder <karankr.das.cse22@iitbhu.ac.in>
2024-12-03 23:31:43 +0530

    feat: implement invoice dashboard

--- app/custom-dashboard/page.tsx
+++ app/custom-dashboard/page.tsx
@@ -12,24 +12,72 @@ export default function InvoiceDashboard() {
   const [{ wallet }] = useConnectWallet();
   const { requestNetwork } = useAppContext();
   const [invoices, setInvoices] = useState<any[]>([]);
-
-  const requestClient = new RequestNetwork({
-    nodeConnectionConfig: {
-      baseURL: 'https://sepolia.gateway.request.network/',
-    },
-  });
+  const [invoicesByCurrency, setInvoicesByCurrency] = useState<{
+    [key: string]: any[];
+  }>({});
+  const [borrowedMoney, setBorrowedMoney] = useState<{ [key: string]: number }>(
+    {}
+  );
+  const [depositedMoney, setDepositedMoney] = useState<{ [key: string]: number }>(
+    {}
+  );
 
   useEffect(() => {
     if (wallet) {
       requestNetwork
         ?.fromIdentity({
           type: Types.Identity.TYPE.ETHEREUM_ADDRESS,
-          value: wallet?.accounts[0].address as string,
+          value: wallet?.accounts[0].address.toLowerCase() as string,
         })
         .then((requests) => {
           const requestDatas = requests.map((request) => request.getData());
           setInvoices(requestDatas);
-          //   console.log('requestDatas', requestDatas);
+
+          const groupedByCurrency = requestDatas.reduce((acc, invoice) => {
+            const currency = invoice.currencyInfo.type;
+            if (!acc[currency]) {
+              acc[currency] = [];
+            }
+            acc[currency].push(invoice);
+            return acc;
+          }, {} as { [key: string]: any[] });
+
+          setInvoicesByCurrency(groupedByCurrency);
+          console.log('groupedByCurrency', groupedByCurrency);
+
+          const borrowedMoneyTemp = requestDatas.reduce((acc, invoice) => {
+            const currency = invoice.currencyInfo.type;
+            if (
+              invoice.payee?.value.toLowerCase() === wallet?.accounts[0].address.toLowerCase()
+            ) {
+              console.log('amount', invoice.expectedAmount);
+              if (!acc[currency]) {
+                acc[currency] = 0;
+              }
+              acc[currency] += Number(invoice.expectedAmount);
+            }
+            return acc;
+          }, {} as { [key: string]: number });
+
+          setBorrowedMoney(borrowedMoneyTemp);
+          console.log('borrowedMoney', borrowedMoneyTemp);
+
+          const depositedMoneyTemp = requestDatas.reduce((acc, invoice) => {
+            const currency = invoice.currencyInfo.type;
+            if (
+              invoice.payer?.value.toLowerCase() === wallet?.accounts[0].address.toLowerCase()
+            ) {
+              console.log('amount', invoice.expectedAmount);
+              if (!acc[currency]) {
+                acc[currency] = 0;
+              }
+              acc[currency] += Number(invoice.expectedAmount);
+            }
+            return acc;
+          }, {} as { [key: string]: number });
+
+          setDepositedMoney(depositedMoneyTemp);
+          console.log('depositedMoney', depositedMoneyTemp);
         });
     }
   }, [wallet, requestNetwork]);
@@ -37,33 +85,33 @@ export default function InvoiceDashboard() {
   return (
     <div className='container m-auto w-[100%] h-screen'>
       <Navbar />
-      <div className='mt-8'>
-        <table className='min-w-full bg-white'>
+      <div className='mt-8 flex justify-center'>
+        <table className='min-w-full bg-white text-center'>
           <thead>
             <tr>
-              <th className='py-2 px-4 border-b'>Created</th>
-              <th className='py-2 px-4 border-b'>Invoice #</th>
-              <th className='py-2 px-4 border-b'>Payee</th>
-              <th className='py-2 px-4 border-b'>Payer</th>
-              <th className='py-2 px-4 border-b'>Expected Amount</th>
-              <th className='py-2 px-4 border-b'>Payment Network</th>
-              <th className='py-2 px-4 border-b'>Status</th>
+              <th className='py-2 px-4 border-b'>Currency</th>
+              <th className='py-2 px-4 border-b'>Amount</th>
+              <th className='py-2 px-4 border-b'>Actions</th>
             </tr>
           </thead>
           <tbody>
-            {invoices.map((invoice, index) => (
+            {Object.keys(invoicesByCurrency).map((currency, index) => (
               <tr key={index}>
+                <td className='py-2 px-4 border-b'>{currency}</td>
                 <td className='py-2 px-4 border-b'>
-                  {invoice.contentData.creationDate}
+                  {Math.abs((depositedMoney[currency] || 0) - (borrowedMoney[currency] || 0))}
                 </td>
                 <td className='py-2 px-4 border-b'>
-                  {invoice.contentData.invoiceNumber}
+                  {(depositedMoney[currency] || 0) > (borrowedMoney[currency] || 0) ? (
+                    <button className='bg-red-500 text-white py-1 px-2 rounded'>
+                      Withdraw
+                    </button>
+                  ) : (
+                    <button className='bg-blue-500 text-white py-1 px-2 rounded'>
+                      Deposit
+                    </button>
+                  )}
                 </td>
-                <td className='py-2 px-4 border-b'>{invoice.payee.value}</td>
-                <td className='py-2 px-4 border-b'>{invoice.payer.value}</td>
-                <td className='py-2 px-4 border-b'>{invoice.expectedAmount}</td>
-                <td className='py-2 px-4 border-b'>{invoice.cuurrency}</td>
-                <td className='py-2 px-4 border-b'>{invoice.status}</td>
               </tr>
             ))}
           </tbody>
